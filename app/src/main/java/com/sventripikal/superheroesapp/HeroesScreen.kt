54bda880e88d39ff0b913402620080bcc18bb9e4
Jose Garza <sventripikal@gmail.com>
2024-09-14 15:10:00 -0500

    update surface & list layout
    -added SuperHeroesApp composable
    -applied surface & lazy column to display list
    -updated comments
    -added light & dark app previews
    -removed List parameter from SuperHeroesApp composable

--- app/src/main/java/com/sventripikal/superheroesapp/HeroesScreen.kt
+++ app/src/main/java/com/sventripikal/superheroesapp/HeroesScreen.kt
@@ -6,28 +6,77 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.sventripikal.superheroesapp.data.HeroesData
 import com.sventripikal.superheroesapp.model.Hero
 import com.sventripikal.superheroesapp.ui.theme.SuperHeroesAppTheme
+
+/**
+ *  @param modifier: modifiers applied to composable
+ */
+@Composable
+fun SuperHeroesApp(             // composable displaying heroes using LazyColumn
+    modifier: Modifier = Modifier
+) {
+    Surface(    // apply app background surface
+        modifier = modifier.fillMaxSize()
+    ) {
+        // get heroes list
+        val listOfHeroes = HeroesData.heroesList
+
+        //  get LocalLayoutDirection
+        val layoutDirection = LocalLayoutDirection.current
+
+        LazyColumn(     // LazyColumn to display hero list
+            contentPadding = PaddingValues(dimensionResource(R.dimen.sixteen_dp)),  // 16dp spacing around content
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.eight_dp)), // 8dp spacing between items
+            modifier = Modifier
+                .statusBarsPadding()        // top statusBar padding
+                .navigationBarsPadding()    // bottom navigationBar padding
+                .padding(
+                    start = WindowInsets.safeDrawing
+                        .asPaddingValues()
+                        .calculateStartPadding(layoutDirection),  // left screen edge padding
+                    end = WindowInsets.safeDrawing
+                        .asPaddingValues()
+                        .calculateEndPadding(layoutDirection)     // right screen edge padding
+                )
+        ) {
+            items(listOfHeroes) { hero ->
+                HeroListItem(hero)  // create Hero card for each item in list
+            }
+        }
+    }
+}
+
 
 /**
  *  @param hero: hero item containing hero information
@@ -53,8 +102,8 @@ fun HeroListItem(   // composable displaying hero info on a card
 
             Spacer(Modifier.width(dimensionResource(R.dimen.sixteen_dp)))   // 16dp horizontal spacer
 
-            Image(
-                painter = painterResource(hero.image),  // hero image
+            Image(  // hero image
+                painter = painterResource(hero.image),
                 contentDescription = null,
                 modifier = Modifier
                     .size(dimensionResource(R.dimen.seventy_two_dp))    // 72dp image size
@@ -75,39 +124,39 @@ fun HeroInfoColumn(     // composable containing hero name & description
     modifier: Modifier = Modifier
 ) {
     Column(
-        verticalArrangement = Arrangement.Center,   // center text vertically
-        modifier = modifier.fillMaxSize()   // fill entire empty space
+        modifier = modifier.fillMaxSize()   // fill 3/4 card space & entire column space
     ) {
-        Text(
+        Text(   // hero name
             text = stringResource(hero.name),
-            style = MaterialTheme.typography.displaySmall
+            style = MaterialTheme.typography.displaySmall,
+            overflow = TextOverflow.Visible     // avoid text clipping
         )
 
-        Text(
+        Text(   // hero description
             text = stringResource(hero.description),
-            style = MaterialTheme.typography.bodyLarge
+            style = MaterialTheme.typography.bodyLarge,
+            overflow = TextOverflow.Visible     // avoid text clipping
         )
     }
 }
 
 
-/**
- *  @param listOfHeroes: list of Hero items
- *  @param modifier: modifiers applied to composable
- */
+// app Light preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun HeroLazyColumn(             // composable displaying heroes using LazyColumn
-    listOfHeroes: List<Hero>,
-    modifier: Modifier = Modifier
-) {
-    LazyColumn(
-        contentPadding = PaddingValues(dimensionResource(R.dimen.sixteen_dp)),  // 16dp spacing around content
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.eight_dp)), // 8dp spacing between items
-        modifier = modifier
-    ) {
-        items(listOfHeroes) { hero ->
-            HeroListItem(hero)  // create Hero card for each item
-        }
+fun HeroAppLightPreview() {
+    SuperHeroesAppTheme(darkTheme = false) {
+        SuperHeroesApp()
+    }
+}
+
+
+// app Dark preview
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HeroAppDarkPreview() {
+    SuperHeroesAppTheme(darkTheme = true) {
+        SuperHeroesApp()
     }
 }
 
@@ -118,15 +167,5 @@ fun HeroLazyColumn(             // composable displaying heroes using LazyColumn
 fun HeroListItemPreview() {
     SuperHeroesAppTheme(darkTheme = true) {
         HeroListItem(HeroesData.heroesList[0])
-    }
-}
-
-
-// Hero list displayed using LazyColumn preview
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun HeroLazyColumnPreview() {
-    SuperHeroesAppTheme(darkTheme = false) {
-        HeroLazyColumn(HeroesData.heroesList)
     }
 }
--- app/src/main/java/com/sventripikal/superheroesapp/MainActivity.kt
+++ app/src/main/java/com/sventripikal/superheroesapp/MainActivity.kt
@@ -4,12 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.sventripikal.superheroesapp.ui.theme.SuperHeroesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,22 +12,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperHeroesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SuperHeroesApp()
             }
         }
     }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
